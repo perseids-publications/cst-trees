cbe09994dbd105a56eb582dfb24d4e5171945aa1
zfletch <zfletch2@gmail.com>
2019-06-07 11:08:22 -0400

    Updates

---
+++ precache-manifest.52cf59a1e2504cadf9a1d5f05637e977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75dcf0846554dc1826b5e2fc6c80ec74",
+    "url": "/cst-trees/index.html"
+  },
+  {
+    "revision": "655c614efbbaf9367eaf",
+    "url": "/cst-trees/static/css/2.ec3f0427.chunk.css"
+  },
+  {
+    "revision": "f31f580811a4e70efe20",
+    "url": "/cst-trees/static/css/main.a730fb68.chunk.css"
+  },
+  {
+    "revision": "655c614efbbaf9367eaf",
+    "url": "/cst-trees/static/js/2.21d3e77f.chunk.js"
+  },
+  {
+    "revision": "f31f580811a4e70efe20",
+    "url": "/cst-trees/static/js/main.7b113e74.chunk.js"
+  },
+  {
+    "revision": "63fa2372b77ac504fcaf",
+    "url": "/cst-trees/static/js/runtime~main.c92236a4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cst-trees/precache-manifest.944f030041002abfdca27b1f3c728636.js"
+  "/cst-trees/precache-manifest.52cf59a1e2504cadf9a1d5f05637e977.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cst-trees/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cst-trees/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
